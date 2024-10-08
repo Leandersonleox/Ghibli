4b4c16ee21c2bbd7b26d032b1b2a9af1b80ca18e
Leandersonleox <167826248+Leandersonleox@users.noreply.github.com>
2024-10-08 14:45:13 -0300

    add arquivos

---
+++ js/ghibli.js
@@ -0,0 +1,97 @@
+document.getElementById("nausicaa").onclick = function() {
+    window.location.href = "./paginas/nausicaa.html";
+};
+
+document.getElementById("castelonoceu").onclick = function() {
+    window.location.href = "./paginas/casteloceu.html";
+};
+
+document.getElementById("meuamigototoro").onclick = function() {
+    window.location.href = "./paginas/totoro.html";
+};
+
+document.getElementById("tumulodosvagalumes").onclick = function() {
+    window.location.href = "./paginas/tumulovagalumes.html";
+};
+
+document.getElementById("kiki").onclick = function() {
+    window.location.href = "./paginas/kiki.html";
+};
+
+document.getElementById("memoriasdeontem").onclick = function() {
+    window.location.href = "./paginas/memoriasontem.html";
+};
+
+document.getElementById("porcorosso").onclick = function() {
+    window.location.href = "./paginas/porcorosso.html";
+};
+
+document.getElementById("eupossoouvirooceano").onclick = function() {
+    window.location.href = "./paginas/ouviroceano.html";
+};
+
+document.getElementById("sussurrosdocoracao").onclick = function() {
+    window.location.href = "./paginas/sussurroscoracao.html";
+};
+
+document.getElementById("princesamononoke").onclick = function() {
+    window.location.href = "./paginas/mononoke.html";
+};
+
+document.getElementById("meusvizinhosyamadas").onclick = function() {
+    window.location.href = "./paginas/vizinhosyamadas.html";
+};
+
+document.getElementById("aviagemdechihiro").onclick = function() {
+    window.location.href = "./paginas/chihiro.html";
+};
+
+document.getElementById("reinodosgatos").onclick = function() {
+    window.location.href = "./paginas/reinogatos.html";
+};
+
+document.getElementById("casteloanimado").onclick = function() {
+    window.location.href = "./paginas/casteloanimado.html";
+};
+
+document.getElementById("contosdeterramar").onclick = function() {
+    window.location.href = "./paginas/terramar.html";
+};
+
+document.getElementById("ponyo").onclick = function() {
+    window.location.href = "./paginas/ponyo.html";
+};
+
+document.getElementById("mundodospequeninos").onclick = function() {
+    window.location.href = "./paginas/pequeninos.html";
+};
+
+document.getElementById("colinakokuriko").onclick = function() {
+    window.location.href = "./paginas/kokuriko.html";
+};
+
+document.getElementById("vidasaovento").onclick = function() {
+    window.location.href = "./paginas/vidasaovento.html";
+};
+
+document.getElementById("princesakaguya").onclick = function() {
+    window.location.href = "./paginas/kaguya.html";
+};
+
+document.getElementById("asmemoriasdemarnie").onclick = function() {
+    window.location.href = "./paginas/memoriasdemarnie.html";
+};
+
+document.getElementById("ayaeabruxa").onclick = function() {
+    window.location.href = "./paginas/ayaeabruxa.html";
+};
+
+document.getElementById("omeminoeagarca").onclick = function() {
+    window.location.href = "./paginas/meninoeagarca.html";
+};
+
+document.getElementById("pompoko").onclick = function() {
+    window.location.href = "./paginas/ponpoko.html";
+};
+
+
